ed9064431ed01f8c6453e7cbe8bbfaea6fdeb1ae
David Stauffer <david.stauffer@williams-forrest.com>
2016-07-25 17:06:58 -0400

    initial commit

---
+++ show-credentials.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+ROLE=`curl -s http://169.254.169.254/latest/meta-data/iam/security-credentials/`
+echo curl -s http://169.254.169.254/latest/meta-data/iam/security-credentials/$ROLE
+curl -s http://169.254.169.254/latest/meta-data/iam/security-credentials/$ROLE
+echo
